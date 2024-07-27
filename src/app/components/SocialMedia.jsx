4193d903e87825acf4d8b04725eb230e5f1551c9
William Qian <william.z.qian@gmail.com>
2024-07-27 00:53:48 -0400

    Update SocialMedia

--- src/app/components/SocialMedia.jsx
+++ src/app/components/SocialMedia.jsx
@@ -3,16 +3,14 @@
 import personalInfo from '../../../data/personalInfo.json';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {
-    faFile,
-} from '@fortawesome/free-solid-svg-icons';
-
-import {
     faLinkedin,
     faGithub,
     faXTwitter,
     faInstagram,
     faFacebook,
-    faGoogleScholar
+    faGoogleScholar,
+    faOrcid,
+    faKaggle
 } from '@fortawesome/free-brands-svg-icons';
 
 const Box = ({href, label, onClick, children}) => (
@@ -29,39 +27,44 @@ const Box = ({href, label, onClick, children}) => (
 const SocialMedia = () => {
     return (
         <div className="flex flex-wrap justify-between max-w-full">
-            {personalInfo.social_media.cv && (
-                <Box href={personalInfo.social_media.cv} label="CV">
-                    <FontAwesomeIcon icon={faFile} size="2xl" style={{ color: "#000000" }} />
-                </Box>
-            )}
             {personalInfo.social_media.linkedin && (
                 <Box href={personalInfo.social_media.linkedin} label="LinkedIn">
-                    <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{ color: "#000000" }} />
-                </Box>
-            )}
-            {personalInfo.social_media.github && (
-                <Box href={personalInfo.social_media.github} label="Github">
-                    <FontAwesomeIcon icon={faGithub} size="2xl" style={{ color: "#000000" }} />
+                    <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{color: "#000000"}}/>
                 </Box>
             )}
             {personalInfo.social_media.X && (
                 <Box href={personalInfo.social_media.X} label="X">
-                    <FontAwesomeIcon icon={faXTwitter} size="2xl" style={{ color: "#000000" }} />
+                    <FontAwesomeIcon icon={faXTwitter} size="2xl" style={{color: "#000000"}}/>
                 </Box>
             )}
             {personalInfo.social_media.instagram && (
                 <Box href={personalInfo.social_media.instagram} label="Instagram">
-                    <FontAwesomeIcon icon={faInstagram} size="2xl" style={{ color: "#000000" }} />
+                    <FontAwesomeIcon icon={faInstagram} size="2xl" style={{color: "#000000"}}/>
                 </Box>
             )}
             {personalInfo.social_media.facebook && (
                 <Box href={personalInfo.social_media.facebook} label="Facebook">
-                    <FontAwesomeIcon icon={faFacebook} size="2xl" style={{ color: "#000000" }} />
+                    <FontAwesomeIcon icon={faFacebook} size="2xl" style={{color: "#000000"}}/>
+                </Box>
+            )}
+            {personalInfo.social_media.github && (
+                <Box href={personalInfo.social_media.github} label="Github">
+                    <FontAwesomeIcon icon={faGithub} size="2xl" style={{color: "#000000"}}/>
                 </Box>
             )}
             {personalInfo.social_media.google_scholar && (
                 <Box href={personalInfo.social_media.google_scholar} label="Google Scholar">
-                    <FontAwesomeIcon icon={faGoogleScholar} size="2xl" style={{ color: "#000000" }} />
+                    <FontAwesomeIcon icon={faGoogleScholar} size="2xl" style={{color: "#000000"}}/>
+                </Box>
+            )}
+            {personalInfo.social_media.orcid && (
+                <Box href={personalInfo.social_media.orcid} label="ORCID">
+                    <FontAwesomeIcon icon={faOrcid} size="2xl" style={{color: "#000000",}}/>
+                </Box>
+            )}
+            {personalInfo.social_media.kaggle && (
+                <Box href={personalInfo.social_media.kaggle} label="Kaggle">
+                    <FontAwesomeIcon icon={faKaggle} size="2xl" style={{color: "#000000",}}/>
                 </Box>
             )}
         </div>
