6a95af47b3093c1d491cc313ae5360713c18953f
William Qian <william.z.qian@gmail.com>
2024-07-27 00:25:13 -0400

    Update metadata

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Layout from "@/app/components/Content";
 
-import { config} from '@fortawesome/fontawesome-svg-core'
+import {config} from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Content from "@/app/components/Content";
+
 config.autoAddCss = false
 
 export default function Home() {
-  return (
-    <Content></Content>
-  );
+    return (
+        <Content></Content>
+    );
 }
