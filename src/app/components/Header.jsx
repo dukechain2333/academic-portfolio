3e420b4afd4c03ca11be195ccf189a7558e88935
William Qian <william.z.qian@gmail.com>
2024-07-28 00:11:12 -0400

    Fixed the problem of header becomes transparent in Safari

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,10 @@
 import NavLink from './NavLink';
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {useState} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
 import personalInfo from '../../../data/personalInfo.json';
 
-function Header({ scrolled }) {
+function Header({scrolled}) {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -13,18 +13,18 @@ function Header({ scrolled }) {
 
     return (
         <header
-            className={`z-10 pt-10 pb-1 mb-10 pl-0 top-0 right-0 left-0 transition border-b ${scrolled ? 'border-gray-400' : 'bg-transparent border-transparent'} sticky w-full backdrop-filter backdrop-blur-md`}>
-            <div className="container mx-auto flex justify-between items-center pb-9 px-5 w-full max-w-5xl">
+            className={`z-10 pt-4 pb-4 mb-4 pl-0 top-0 right-0 left-0 transition border-b ${scrolled ? 'border-gray-400' : 'bg-transparent border-transparent'} sticky w-full backdrop-blur-md`}>
+            <div className="container mx-auto flex justify-between items-center px-5 w-full max-w-5xl">
                 <div className="flex items-center space-x-4">
                     <nav className="hidden md:flex items-center space-x-4">
-                        <NavLink title="About" href="/" />
-                        <NavLink title="Publications" href="/publications" />
-                        <NavLink title="Research" href="/research" />
-                        <NavLink title="Projects" href="/projects" />
+                        <NavLink title="About" href="/"/>
+                        <NavLink title="Publications" href="/publications"/>
+                        <NavLink title="Research" href="/research"/>
+                        <NavLink title="Projects" href="/projects"/>
                     </nav>
                     <div className="md:hidden">
                         <button onClick={toggleMenu}>
-                            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="2x" />
+                            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="2x"/>
                         </button>
                     </div>
                 </div>
@@ -34,17 +34,15 @@ function Header({ scrolled }) {
             </div>
             {isOpen && (
                 <nav className="md:hidden flex flex-col items-start space-y-4 p-4">
-                    <NavLink title="About" href="/" />
-                    <NavLink title="Publications" href="/publications" />
-                    <NavLink title="Research" href="/research" />
-                    <NavLink title="Projects" href="/projects" />
+                    <NavLink title="About" href="/"/>
+                    <NavLink title="Publications" href="/publications"/>
+                    <NavLink title="Research" href="/research"/>
+                    <NavLink title="Projects" href="/projects"/>
                 </nav>
             )}
         </header>
     );
 }
-
-
 
 
 export default Header;
