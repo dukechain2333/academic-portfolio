a3a25a4622527046e47c323c1e72f166b06461bf
William Qian <william.z.qian@gmail.com>
2024-07-28 00:32:21 -0400

    News not support links.

--- src/app/components/News.jsx
+++ src/app/components/News.jsx
@@ -3,6 +3,9 @@
 import React from "react";
 import news from '../../../data/news.json';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+
 export default function News() {
     const sortedNews = news.news.sort((a, b) => new Date(b.time) - new Date(a.time));
 
@@ -12,11 +15,21 @@ export default function News() {
                 Recent News
             </div>
             {sortedNews.map(item => (
-                <div key={item.id} className="mb-4">
-                    <div className="text-sm text-gray-500">{item.time}</div>
-                    <div className="text-lg">{item.content}</div>
-                </div>
+                item.href ? (
+                    <a key={item.id} href={item.href} className="block relative mb-4 p-4 rounded-lg hover:bg-amber-100 hover:shadow-lg transition active:bg-amber-200">
+                        <div className="absolute bottom-2 right-2">
+                            <FontAwesomeIcon icon={faExternalLinkAlt} className="text-gray-600" />
+                        </div>
+                        <div className="text-sm text-gray-500">{item.time}</div>
+                        <div className="text-lg">{item.content}</div>
+                    </a>
+                ) : (
+                    <div key={item.id} className="relative mb-4 p-4">
+                        <div className="text-sm text-gray-500">{item.time}</div>
+                        <div className="text-lg">{item.content}</div>
+                    </div>
+                )
             ))}
         </div>
-    )
+    );
 }
